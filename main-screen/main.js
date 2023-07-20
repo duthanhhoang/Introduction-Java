6e9a4ffd3a7c72b4d5517e31f1c0ae32939310e8
Dư Thanh Hoàng <duthanhhoang@gmail.com>
2023-07-20 14:45:01 +0700

    Add files via upload

---
+++ main-screen/main.js
@@ -0,0 +1,12 @@
+
+let student_1 = new Student(1,"Xiao_Zhan.jpeg","Loan", 110899,"11A1");
+let student_2 = new Student(2,"Xiao_Zhan.jpeg","Loan", 110899,"11A2");
+let list = new StudentList();
+
+list.addStudent(student_1);
+list.addStudent(student_2);
+// list.addStudent(student_2);
+console.log(list.students)
+list.displayAll();
+ 
+// document.getElementById('add-btn').addEventListener('click', function(){list.addStudent(student_1);});
